a548253d4b2d5da4dee60d2d72ddb88e69f74936
Soham Debnath <71327469+Popo047@users.noreply.github.com>
2022-12-12 11:58:57 +0530

    Delete database.js

--- server/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("posts", "root", "Abepopo007!", {
-	dialect: "mysql",
-	host: "localhost",
-});
-
-module.exports = sequelize;
